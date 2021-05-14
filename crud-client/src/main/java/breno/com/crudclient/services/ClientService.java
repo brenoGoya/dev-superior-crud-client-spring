877759f2fa662d6b6dd08637752f5b9b313476f4
Breno Goya <breno.goya@gmail.com>
2021-05-14 15:53:10 -0300

    Find by Id and Errors handler

--- crud-client/src/main/java/breno/com/crudclient/services/ClientService.java
+++ crud-client/src/main/java/breno/com/crudclient/services/ClientService.java
@@ -1,5 +1,7 @@
 package breno.com.crudclient.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import breno.com.crudclient.dto.ClientDTO;
 import breno.com.crudclient.entities.Client;
 import breno.com.crudclient.repositories.ClientRepository;
+import breno.com.crudclient.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ClientService {
@@ -20,6 +23,14 @@ public class ClientService {
 	public Page<ClientDTO> findAllPaged (PageRequest pageRequest) {
 		Page<Client> list = repository.findAll(pageRequest);
 		return list.map(x -> new ClientDTO(x));
+	}
+
+	@Transactional(readOnly = true)
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		
+		Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new ClientDTO(entity);		
 	}
 	
 }
---
+++ crud-client/src/main/java/breno/com/crudclient/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package breno.com.crudclient.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public DatabaseException (String msg) {
+		super(msg);
+	}
+}
