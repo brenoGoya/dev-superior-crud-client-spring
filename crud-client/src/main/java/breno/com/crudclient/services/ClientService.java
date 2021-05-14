c4ffbd2e0f74d207b8386aed6dc3a3362b2d70ea
Breno Goya <breno.goya@gmail.com>
2021-05-14 17:00:32 -0300

    Update client by id, PUT method

--- crud-client/src/main/java/breno/com/crudclient/services/ClientService.java
+++ crud-client/src/main/java/breno/com/crudclient/services/ClientService.java
@@ -2,6 +2,8 @@ package breno.com.crudclient.services;
 
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -33,19 +35,33 @@ public class ClientService {
 		return new ClientDTO(entity);		
 	}
 
+	@Transactional
 	public ClientDTO insert(ClientDTO dto) {
 		Client entity = new Client();
-		copyToEntity(dto, entity);
+		copyDtoToEntity(dto, entity);
 		entity = repository.save(entity);
-		return new ClientDTO(entity);
-		
+		return new ClientDTO(entity);		
 	}	
 	
-	private void copyToEntity(ClientDTO dto, Client entity) {
+	@Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getOne(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+	
+	private void copyDtoToEntity(ClientDTO dto, Client entity) {
 		entity.setName(dto.getName());
 		entity.setCpf(dto.getCpf());
 		entity.setIncome(dto.getIncome());
 		entity.setBirthDate(dto.getBirthDate());
 		entity.setChildren(dto.getChildren());		
 	}
+	
 }
